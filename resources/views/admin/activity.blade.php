026f10161617402d8ab2b8fe34e6557e5f123e41
Thekingdx <fakhriprogamers@gmail.com>
2024-06-26 10:59:41 +0700

    edit-modal

--- resources/views/admin/activity.blade.php
+++ resources/views/admin/activity.blade.php
@@ -21,7 +21,7 @@
     </div> <!--end::App Content Header--> <!--begin::App Content-->
     <div class="app-content"> <!--begin::Container-->
         <div class="container-fluid"> <!--begin::Row-->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-modal">
                 Tambah Aktifitas
             </button>
             <div class="row">
@@ -36,7 +36,9 @@
                                     <small class="text-muted">{{ $act->date }}</small>
                                 </div>
                                 <div class="card-footer">
-                                    <button class="btn btn-outline-info"><i class="bi bi-pencil"></i></button>
+                                    <button class="btn btn-outline-info" data-bs-toggle="modal"
+                                        data-bs-target="#edit-modal{{ $act->id }}"><i
+                                            class="bi bi-pencil"></i></button>
                                     <button class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
                                 </div>
                             </div>
@@ -46,26 +48,27 @@
             </div>
         </div> <!--end::Container-->
     </div> <!--end::App Content-->
-    <!-- Modal -->
+    <!-- Add Modal -->
     <form action="/admin/activity/create" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="add-modalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Aktifitas</h1>
+                        <h1 class="modal-title fs-5" id="add-modalLabel">Tambah Aktifitas</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="mb-3">
-                            <label for="formFile" class="form-label">Masukkan Gambar</label>
-                            <input class="form-control" type="file" id="formFile" accept="image/*" name="image">
-                            <div id="imageContainer"></div>
+                            <label for="formFile1" class="form-label">Masukkan Gambar</label>
+                            <input class="form-control file-input" type="file" id="formFile1" accept="image/*"
+                                name="image">
+                            <div class="image-container" id="imageContainer1"></div>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com"
+                            <input type="text" class="form-control" id="floatingInput1" placeholder="name@example.com"
                                 name="title">
-                            <label for="floatingInput">Judul</label>
+                            <label for="floatingInput1">Judul</label>
                         </div>
                         <div class="form-floating mb-3">
                             <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="description"
@@ -73,9 +76,9 @@
                             <label for="floatingTextarea2">Deskripsi</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="date" class="form-control" id="floatingInput" name="date"
+                            <input type="date" class="form-control" id="floatingInput2" name="date"
                                 placeholder="name@example.com">
-                            <label for="floatingInput">Tanggal</label>
+                            <label for="floatingInput2">Tanggal</label>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -86,20 +89,69 @@
             </div>
         </div>
     </form>
+    {{-- Edit Modal --}}
+    @foreach ($activities as $act)
+        <form action="/admin/activity/edit/{{ $act->id }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="modal fade" id="edit-modal{{ $act->id }}" tabindex="-1" aria-labelledby="edit-modalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="edit-modalLabel">Edit Aktifitas</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="formFile2" class="form-label">Masukkan Gambar</label>
+                                <input class="form-control file-input" type="file" id="formFile2" accept="image/*"
+                                    name="image" multiple>
+                                <div class="image-container" id="imageContainer2"><img
+                                        src="{{ Storage::url($act->images) }}" class="img-fluid img-thumbnail"></div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput3"
+                                    placeholder="name@example.com" name="title" value="{{ $act->title }}">
+                                <label for="floatingInput3">Judul</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea4" name="description"
+                                    style="height: 100px">{{ $act->description }}</textarea>
+                                <label for="floatingTextarea4">Deskripsi</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="date" class="form-control" id="floatingInput4" name="date"
+                                    placeholder="name@example.com" value="{{ $act->date }}">
+                                <label for="floatingInput4">Tanggal</label>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    @endforeach
     <script>
-        formFile.onchange = evt => {
-            const files = formFile.files;
-            const imageContainer = document.getElementById('imageContainer');
-            imageContainer.innerHTML = ''; // Clear previous images
+        document.querySelectorAll('.file-input').forEach(input => {
+            input.addEventListener('change', (evt) => {
+                const files = evt.target.files;
+                const imageContainer = evt.target.closest('.modal-body').querySelector('.image-container');
+                imageContainer.innerHTML = ''; // Clear previous images
 
-            Array.from(files).forEach(file => {
-                if (file) {
-                    const img = document.createElement('img');
-                    img.src = URL.createObjectURL(file);
-                    img.classList.add('img-fluid', 'img-thumbnail');
-                    imageContainer.appendChild(img);
-                }
+                Array.from(files).forEach(file => {
+                    if (file) {
+                        const img = document.createElement('img');
+                        img.src = URL.createObjectURL(file);
+                        img.classList.add('img-fluid',
+                            'img-thumbnail'); // Added margin for better spacing
+                        imageContainer.appendChild(img);
+                    }
+                });
             });
-        }
+        });
     </script>
 @endsection
